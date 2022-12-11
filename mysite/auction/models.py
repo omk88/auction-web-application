346a2585e7343fb98eef15944a4d3a0635fb0ae5
aanQMUL <ec20595@qmul.ac.uk>
2022-12-11 17:24:10 +0000

    Items model
    Initial item model implementation, need to configure user foreign key and item image

--- mysite/auction/models.py
+++ mysite/auction/models.py
@@ -1,3 +1,17 @@
+import os
 from django.db import models
+from django.conf import settings
+#from mysite.users.models import models
 
-# Create your models here.
+def image_path():
+    return os.path.join(settings.LOCAL_FILE_DIR, 'images')
+
+class Item(models.Model): 
+    user = models.ForeignKey(User)
+    title = models.CharField(max_length=100)
+    starting_price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(max_length=600, default='', blank=True)
+    item_image = models.FilePathField(path=image_path)
+    end_datetime = models.DateTimeField()
+    def __str__(self):
+        return "{self.title}, {self.description}"
