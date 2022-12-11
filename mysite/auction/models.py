541d7b9bb1f9b2a5c71b3ebb16edd3e22db6e797
aanQMUL <ec20595@qmul.ac.uk>
2022-12-11 20:47:50 +0000

    Added comment model and other dictionaries

--- mysite/auction/models.py
+++ mysite/auction/models.py
@@ -13,13 +13,48 @@ class Item(models.Model):
     description = models.TextField(max_length=600, default='', blank=True)
     item_image = models.FilePathField(path=image_path)
     end_datetime = models.DateTimeField()
+
     def __str__(self):
         return "{self.title}, {self.description}"
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user': self.user,
+            'title': self.title,
+            'starting_price': self.starting_price,
+            'description': self.description,
+            'item_image': self.item_image,
+            'end_datetime': self.end_datetime
+        }
 
 class Bid(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User)
-    items = models.ForeignKey(Item)
+    item = models.ForeignKey(Item)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
+
     def __str__(self):
         return self.amount
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user': self.user,
+            'item': self.item,
+            'amount': self.amount
+        }
+
+class Comment(models.Model):
+    user = models.ForeignKey(User)
+    comment = description = models.TextField(max_length=600, default='', blank=True)
+
+    def __str__(self):
+        return self.comment
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user': self.user,
+            'comment': self.comment,
+        }
