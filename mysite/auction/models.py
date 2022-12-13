e2ef3632b63bbb18309557b4d2cadad971ae056e
aanQMUL <ec20595@qmul.ac.uk>
2022-12-13 12:31:35 +0000

    Updated foreign key fields

--- mysite/auction/models.py
+++ mysite/auction/models.py
@@ -1,13 +1,13 @@
 import os
 from django.db import models
 from django.conf import settings
-from mysite.users.models import models
+from users.models import models
 
 def image_path():
     return os.path.join(settings.LOCAL_FILE_DIR, 'images')
 
 class Item(models.Model): 
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     starting_price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(max_length=600, default='', blank=True)
@@ -30,8 +30,8 @@ class Item(models.Model):
 
 class Bid(models.Model):
     id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User)
-    item = models.ForeignKey(Item)
+    user = models.ForeignKey(User, related_name='bids', on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, related_name='bids', on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
