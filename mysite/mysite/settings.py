beeac203442ea18f88561fbf67a1470ff766ca3f
aanQMUL <78413661+aanQMUL@users.noreply.github.com>
2022-12-14 16:49:06 +0000

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'users',
     'accounts',
+    'auction',
 ]
 
 AUTH_USER_MODEL = 'users.CustomUser'
@@ -46,12 +48,26 @@ AUTH_USER_MODEL = 'users.CustomUser'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
+CORS_ALLOWED_ORIGINS = [
+"http://localhost:5173",
+"http://127.0.0.1:5173",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+"http://localhost:5173",
+"http://127.0.0.1:5173",
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'mysite.urls'
 
