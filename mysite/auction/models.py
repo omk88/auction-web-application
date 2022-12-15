6a3108125a34bd75ea0a99fc8881634b295af7e8
aanQMUL <ec20595@qmul.ac.uk>
2022-12-15 05:07:03 +0000

    Updating models

--- mysite/auction/models.py
+++ mysite/auction/models.py
@@ -55,7 +55,9 @@ class Bid(models.Model):
 
 class Question(models.Model):
     user = models.ForeignKey(CustomUser, related_name='questions', on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, related_name='questions', on_delete=models.CASCADE, null=True)
     question = models.TextField(max_length=600, default='', blank=True)
+    
 
     def __str__(self):
         return self.question
@@ -64,11 +66,14 @@ class Question(models.Model):
         return {
             'id': self.id,
             'user': self.user,
-            'question': self.question,
+            'item': self.item,
+            'queston': self.question,
         }
 
 class Answer(models.Model):
     user = models.ForeignKey(CustomUser, related_name='answers', on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, related_name='answers', on_delete=models.CASCADE, null=True)
+    question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE, null=True)
     answer = models.TextField(max_length=600, default='', blank=True)
 
     def __str__(self):
@@ -78,5 +83,7 @@ class Answer(models.Model):
         return {
             'id': self.id,
             'user': self.user,
+            'item': self.item,
+            'question': self.question,
             'answer': self.answer,
         }
--- mysite/auction/urls.py
+++ mysite/auction/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import items_api, item_api, search_api
+from .views import items_api, item_api, search_api, bid_api, question_api, answer_api
 
 urlpatterns = [
     path('api/items/', items_api),
     path('api/item/<int:item_id>/', item_api),
     path('api/search/', search_api),
+    path('api/bid/', bid_api),
+    path('api/question/', question_api),
+    path('api/answer/', answer_api),
 ]
